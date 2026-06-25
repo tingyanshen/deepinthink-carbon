8cfde30735db644c4a608be99ccb8b7bb8cfe244
Tingyan Shen <tingyan.shen@gmail.com>
2026-06-25 15:49:08 +0900

    GH-19 Integrating @openzeppelin openzeppelin-contracts v5.6.1 (#20)

--- specification/benchmark/solidity/ERC20.sol
+++ specification/benchmark/solidity/ERC20.sol
@@ -1,6 +1,15 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.20;
 
-contract MyERC20 {
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
+contract MyERC20 is ERC20, Ownable {
+    constructor(uint256 initialSupply) ERC20("MyToken", "MTK") Ownable(msg.sender) {
+        _mint(msg.sender, initialSupply);
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
 }
